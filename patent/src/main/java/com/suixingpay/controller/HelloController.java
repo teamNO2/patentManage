ad5c1c2f8fa318d5d7b5a9fa7f16fff7247d4434
sunkeqiang <1830716357@qq.com>
2019-11-18 19:35:09 +0800

    初始化

--- patent/src/main/java/com/suixingpay/controller/HelloController.java
+++ patent/src/main/java/com/suixingpay/controller/HelloController.java
@@ -1,16 +1,20 @@
 package com.suixingpay.controller;
 
-import org.springframework.stereotype.Controller;
+import com.suixingpay.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello(){
-        return "hello";
+    @Autowired
+    private HelloService helloService;
+
+    @GetMapping("/hello/{id}")
+    public String hello(@PathVariable("id") Integer id){
+        String name = helloService.getName(id);
+        return name;
     }
 }
